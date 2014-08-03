e9882bbe9ff19a7a7df42d067446d6d73599ccfb
latish <latish86@bk.ru>
2014-08-03 22:27:43 +0600

    Добавил вывод произвольного меню на главную страницу

--- front-page.php
+++ front-page.php
@@ -1,3 +1,15 @@
-
-
-Главная страница
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php wp_head(); ?>
+        <title><?php bloginfo('name') ?></title> 
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+    </head>
+    <body>
+        <div id="wrap">
+            <div id="content">
+                <?php wp_nav_menu();?>
+            </div>
+        </div>
+    </body>
+</html>
